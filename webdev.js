0db296e8568efaffc790687c4dd5f690478e6e47
Britney Echeverria <britney@Britneys-Air.home>
2020-09-20 21:34:54 -0400

    unpushed: center menu, fixed picture

--- webdev.js
+++ webdev.js
@@ -4,15 +4,15 @@ let bake3 = ["Pastries","Strawberry","$7","01:00 PM","01:53 PM","25-Jan-21","19-
 let bake4 = ["Bread","Oat","$3","04:00 PM","04:53 PM","25-Jan-21","19-May-21","Home Delivery?"];																								
 let bake5 = ["Cupcakes","cherry","$3","05:30 PM","06:23 PM","25-Jan-21","19-May-21","Home Delivery?"];																								
 let bake6 = ["Cinnamon rolls","Apple-pie filling","$6","12:00 PM","12:53 PM","25-Jan-21","19-May-21","Home Delivery?"];																							
-function link(array) {
-    var itenary = document.createElement('ul');		// Create the itenary element:
 
+function link(array) {
+    var menu = document.createElement('ul');	// Create A Menu
     for(var i = 0; i < array.length; i++) {
-        var stuff = document.createElement('li');				// Create the itenary stuff:
-        stuff.appendChild(document.createTextNode(array[i]));	// Set its contents:
-        itenary.appendChild(stuff);			// Add it to the itenary:
+        var item = document.createElement('li');	// Create an item 
+        item.appendChild(document.createTextNode(array[i]));	// Set its contents:
+        menu.appendChild(item); // Add item to menu
     }
-    return itenary;				// Finally, return the constructed itenary:
+    return menu;
 }
 
 //Add the contents of bake[0] to #bake:
@@ -23,10 +23,10 @@ document.getElementById("bake4").appendChild(link(bake4));
 document.getElementById("bake5").appendChild(link(bake5));
 document.getElementById("bake6").appendChild(link(bake6));
 
-var foo = document.getElementsByClassName("tog");
+var menuItems = document.getElementsByClassName("tog");
 
-for (var i = 0; i < foo.length; i++) {
-  foo[i].addEventListener("click", function() {
+for (var i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
